22c7928dcd83991a28d116a675b4d6911de05b25
Fredrik Skogstad <fredrik.skogstad94@gmail.com>
2020-03-10 17:12:31 +0100

    Majord updates

--- app.js
+++ app.js
@@ -11,39 +11,44 @@ const addTaskBtn = document.getElementById("add-task-btn");
 const assignTaskBtn = document.getElementById("assign-task-btn");
 const selectName = document.getElementById("name-assigner");
 const selectTask = document.getElementById("task-assigner");
+const removeNameBtn = document.getElementById("remove-name-button");
+const removeTaskBtn = document.getElementById("remove-task-button");
+const removeAssignedBtn = document.getElementById("remove-assigned-button");
 
 // Declare variables
-let storedNameArray = [];
-let storedTaskArray = [];
-let storedAssignedNameArray = [];
-let storedAssignedTaskArray = [];
+let NameArray = [];
+let TaskArray = [];
+let AssignedNameArray = [];
+let AssignedTaskArray = [];
 
 
 /* NAME LIST */
-
 // On click, run the function to add name to array in localStorage
 addNameBtn.addEventListener("click", () => {
-    if (localStorage.storedNameArray != undefined) {
-        storedNameArray = JSON.parse(localStorage.getItem("storedNameArray"));
+    if (name.value.length == 0) {
+        alert("ERROR! Input name.");
+        return false;
+    } else {
+        const nameInput = name.value;
+        NameArray.push(nameInput);
+        localStorage.setItem("NameArray", JSON.stringify(NameArray));
+        name.value = "";
+        createNameListElement();
     }
-    alertUserName();
-    const nameInput = name.value;
-    storedNameArray.push(nameInput);
-    localStorage.setItem("storedNameArray", JSON.stringify(storedNameArray));
-    name.value = "";
-    createNameListElement();
 });
 
 // Append the checkbox to the name-ul element
 // Retrieve values from localStorage, and add it to nameUl
 createNameListElement = () => {
-    const retrievedNameArray = JSON.parse(localStorage.getItem("storedNameArray"));
+    if (localStorage.NameArray) {
+        NameArray = JSON.parse(localStorage.getItem("NameArray"));
+    }
     const li = document.createElement("li");
     const cb = createCheckBox();
     li.appendChild(cb);
-    li.appendChild(document.createTextNode(retrievedNameArray.slice(-1)[0]));
+    li.appendChild(document.createTextNode(NameArray.slice(-1)[0]));
     nameUl.appendChild(li);
-    selectName[selectName.length] = new Option((retrievedNameArray.slice(-1)[0]), (retrievedNameArray.slice(-1)[0]));
+    selectName[selectName.length] = new Option((NameArray.slice(-1)[0]), (NameArray.slice(-1)[0]));
     //Add event listener on click to toggle css strike through class
     cb.addEventListener("click", () => {
         li.classList.toggle("strikethrough");
@@ -53,30 +58,32 @@ createNameListElement = () => {
 
 
 /* TASK LIST */
-
 // On click, run the function to add task to array in localStorage
 addTaskBtn.addEventListener("click", () => {
-    if (localStorage.storedTaskArray != undefined) {
-        storedTaskArray = JSON.parse(localStorage.getItem("storedTaskArray"));
+    if (task.value.length == 0) {
+        alert("ERROR! Input task.");
+        return false;
+    } else {
+        const taskInput = task.value;
+        TaskArray.push(taskInput);
+        localStorage.setItem("TaskArray", JSON.stringify(TaskArray));
+        task.value = "";
+        createTaskListElement();
     }
-    alertUserTask();
-    const taskInput = task.value;
-    storedTaskArray.push(taskInput);
-    localStorage.setItem("storedTaskArray", JSON.stringify(storedTaskArray));
-    task.value = "";
-    createTaskListElement();
 });
 
 // Append the checkbox to the task-ul element
 // Retrieve values from localStorage, and add it to taskUl
 createTaskListElement = () => {
-    const retrievedTaskArray = JSON.parse(localStorage.getItem("storedTaskArray"));
+    if (localStorage.TaskArray) {
+        TaskArray = JSON.parse(localStorage.getItem("TaskArray"));
+    }
     const li = document.createElement("li");
     const cb = createCheckBox();
     li.appendChild(cb);
-    li.appendChild(document.createTextNode(retrievedTaskArray.slice(-1)[0]));
+    li.appendChild(document.createTextNode(TaskArray.slice(-1)[0]));
     taskUl.appendChild(li);
-    selectTask[selectTask.length] = new Option((retrievedTaskArray.slice(-1)[0]), (retrievedTaskArray.slice(-1)[0]));
+    selectTask[selectTask.length] = new Option((TaskArray.slice(-1)[0]), (TaskArray.slice(-1)[0]));
     cb.addEventListener("click", () => {
         li.classList.toggle("strikethrough");
     });
@@ -85,37 +92,36 @@ createTaskListElement = () => {
 
 
 /* ASSIGN TASK FUNCTIONS */
-
 // On click, run the function to add assigned name to array in localStorage
 assignName = () => {
-    if (localStorage.storedAssignedNameArray != undefined) {
-        storedAssignedNameArray = JSON.parse(localStorage.getItem("storedAssignedNameArray"));
+    if (localStorage.AssignedNameArray) {
+        AssignedNameArray = JSON.parse(localStorage.getItem("AssignedNameArray"));
     }
     const nameAssignerInput = nameAssigner.value;
-    storedAssignedNameArray.push(nameAssignerInput);
-    localStorage.setItem("storedAssignedNameArray", JSON.stringify(storedAssignedNameArray));
+    AssignedNameArray.push(nameAssignerInput);
+    localStorage.setItem("AssignedNameArray", JSON.stringify(AssignedNameArray));
 };
 
 // On click, run the function to add assigned task to array in localStorage
 assignTask = () => {
-    if (localStorage.storedAssignedTaskArray != undefined) {
-        storedAssignedTaskArray = JSON.parse(localStorage.getItem("storedAssignedTaskArray"));
+    if (localStorage.AssignedTaskArray) {
+        AssignedTaskArray = JSON.parse(localStorage.getItem("AssignedTaskArray"));
     }
     const taskAssignerInput = taskAssigner.value;
-    storedAssignedTaskArray.push(taskAssignerInput);
-    localStorage.setItem("storedAssignedTaskArray", JSON.stringify(storedAssignedTaskArray));
+    AssignedTaskArray.push(taskAssignerInput);
+    localStorage.setItem("AssignedTaskArray", JSON.stringify(AssignedTaskArray));
 };
 
 // append the checkbox to the taskAssignerUl element
 // retrieve elements from localStorage, and add it to taskAssignerUl
 assignTaskListElement = () => {
-    const retrievedAssignedNameArray = JSON.parse(localStorage.getItem("storedAssignedNameArray"));
-    const retrievedAssignedTaskArray = JSON.parse(localStorage.getItem("storedAssignedTaskArray"));
+    AssignedNameArray = JSON.parse(localStorage.getItem("AssignedNameArray"));
+    AssignedTaskArray = JSON.parse(localStorage.getItem("AssignedTaskArray"));
     const li = document.createElement("li");
     const cb = createCheckBox();
     li.appendChild(cb);
-    li.appendChild(document.createTextNode(`Name: ${retrievedAssignedNameArray.slice(-1)[0]}   -   Task: `));
-    li.appendChild(document.createTextNode(retrievedAssignedTaskArray.slice(-1)[0]));
+    li.appendChild(document.createTextNode(`Name: ${AssignedNameArray.slice(-1)[0]} - Task: `));
+    li.appendChild(document.createTextNode(AssignedTaskArray.slice(-1)[0]));
     taskAssignerUl.appendChild(li);
     cb.addEventListener("click", () => {
         li.classList.toggle("strikethrough");
@@ -125,63 +131,51 @@ assignTaskListElement = () => {
 assignTaskBtn.addEventListener("click", () => {
     assignName();
     assignTask();
-
     assignTaskListElement();
 });
 /* ASSIGN TASK FUNCTIONS - END*/
 
 
-/* COLLECTIVE FUNCTIONS */
-
-// Create checkboxes for li-elements
-createCheckBox = () => {
-    var cb = document.createElement("input");
-    cb.type = "checkbox";
-    cb.checked = false;
-    return cb;
-}
-/* COLLECTIVE FUNCTIONS - END*/
-
-
 //Pull values from local storage and run it on page-load
 loadFromLocaleStorage = () => {
-    if (localStorage.storedNameArray != undefined) {
-        const retrievedNameArray = JSON.parse(localStorage.getItem("storedNameArray"));
-        for (let x of retrievedNameArray) {
+    if (localStorage.NameArray) {
+        NameArray = JSON.parse(localStorage.getItem("NameArray"));
+        for (let x of NameArray) {
             const li = document.createElement("li");
             const cb = createCheckBox();
             li.appendChild(cb);
             li.appendChild(document.createTextNode(x));
             nameUl.appendChild(li);
-            selectName[selectName.length] = new Option(x, retrievedNameArray[x]);
-            cb.addEventListener("click", () => {
-                li.classList.toggle("strikethrough");
-            });
-        }
-    }
-    if (localStorage.storedTaskArray != undefined) {
-        const retrievedTaskArray = JSON.parse(localStorage.getItem("storedTaskArray"));
-        for (let x of retrievedTaskArray) {
-            const li = document.createElement("li");
-            const cb = createCheckBox();
-            li.appendChild(cb);
-            li.appendChild(document.createTextNode(x));
-            taskUl.appendChild(li);
-            selectTask[selectTask.length] = new Option(x, retrievedTaskArray[x]);
+            selectName[selectName.length] = new Option(x, NameArray[x]);
             cb.addEventListener("click", () => {
                 li.classList.toggle("strikethrough");
             });
         }
     }
 
-    if (localStorage.storedAssignedNameArray && localStorage.storedAssignedTaskArray != undefined) {
-        var retrievedAssignedNameArray = JSON.parse(localStorage.getItem("storedAssignedNameArray"));
-        var retrievedAssignedTaskArray = JSON.parse(localStorage.getItem("storedAssignedTaskArray"));
-        for (let x of retrievedAssignedNameArray) {
+    if (localStorage.TaskArray) {
+        TaskArray = JSON.parse(localStorage.getItem("TaskArray"));
+        for (let x of TaskArray) {
             const li = document.createElement("li");
             const cb = createCheckBox();
             li.appendChild(cb);
-            li.appendChild(document.createTextNode(`Name: ${x}   -   Task: ${retrievedAssignedTaskArray}`));
+            li.appendChild(document.createTextNode(x));
+            taskUl.appendChild(li);
+            selectTask[selectTask.length] = new Option(x, TaskArray[x]);
+            cb.addEventListener("click", () => {
+                li.classList.toggle("strikethrough");
+            });
+        }
+    }
+
+    if (localStorage.AssignedNameArray && localStorage.AssignedTaskArray) {
+        AssignedNameArray = JSON.parse(localStorage.getItem("AssignedNameArray"));
+        AssignedTaskArray = JSON.parse(localStorage.getItem("AssignedTaskArray"));
+        for (let x of AssignedNameArray) {
+            const li = document.createElement("li");
+            const cb = createCheckBox();
+            li.appendChild(cb);
+            li.appendChild(document.createTextNode(`Name: ${x} - Task: ${AssignedTaskArray}`));
             taskAssignerUl.appendChild(li);
             cb.addEventListener("click", () => {
                 li.classList.toggle("strikethrough");
@@ -190,55 +184,43 @@ loadFromLocaleStorage = () => {
     }
 }
 
-// Function which alerts user if not filled textbox
-function alertUserName() {
-    if (document.getElementById("name").value.length == 0) {
-        alert("ERROR! Input name.");
-        return false;
-    }
-}
 
-function alertUserTask() {
-    if (document.getElementById("task").value.length == 0) {
-        alert("ERROR! Input task.");
-        return false;
-    }
+/* OTHER FUNCTIONS */
+// Create checkboxes for li-elements
+createCheckBox = () => {
+    const cb = document.createElement("input");
+    cb.type = "checkbox";
+    cb.checked = false;
+    return cb;
 }
-
 
 // On click, which calls three different functions 
-
-document.getElementById("remove-name-button").onclick = function () {
+removeNameBtn.onclick = () => {
     removeLocalName();
     location.reload();
-
 }
 
-document.getElementById("remove-task-button").onclick = function () {
+removeTaskBtn.onclick = () => {
     removeLocalTask();
     location.reload();
 }
 
-document.getElementById("remove-assigned-button").onclick = function () {
+removeAssignedBtn.onclick = () => {
     removeLocalAssigned();
     location.reload();
 }
 
 // Functions witch clears the localStorage of Names, Tasks and Assigned names and tasks.
-
-
-function removeLocalName() {
-    localStorage.removeItem("storedNameArray");
-
+removeLocalName = () => {
+    localStorage.removeItem("NameArray");
 }
 
-function removeLocalTask() {
-    localStorage.removeItem("storedTaskArray");
-
+removeLocalTask = () => {
+    localStorage.removeItem("TaskArray");
 }
 
-function removeLocalAssigned() {
-    localStorage.removeItem("storedAssignedNameArray");
-    localStorage.removeItem("storedAssignedTaskArray");
-
+removeLocalAssigned = () => {
+    localStorage.removeItem("AssignedNameArray");
+    localStorage.removeItem("AssignedTaskArray");
 }
+/* OTHER FUNCTIONS - END*/
